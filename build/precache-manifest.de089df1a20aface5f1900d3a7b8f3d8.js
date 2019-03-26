df44862e51e13bee2882ba16ca186f0f0f47dccf
ercasale97 <ercasale97@gmail.com>
2019-03-25 23:40:17 -0500

    deployed

---
+++ build/precache-manifest.de089df1a20aface5f1900d3a7b8f3d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "14053ae2d7dec7fd8027",
+    "url": "/ercasale97/clicky-game/static/js/runtime~main.8629ee55.js"
+  },
+  {
+    "revision": "f545e5ab4b015fc40a2c",
+    "url": "/ercasale97/clicky-game/static/js/main.ecfc4328.chunk.js"
+  },
+  {
+    "revision": "19d95efcc4ebcd21d2b5",
+    "url": "/ercasale97/clicky-game/static/js/2.fd9fc717.chunk.js"
+  },
+  {
+    "revision": "f545e5ab4b015fc40a2c",
+    "url": "/ercasale97/clicky-game/static/css/main.1fe55148.chunk.css"
+  },
+  {
+    "revision": "a1f0f2b26aed43704ab1627300af8900",
+    "url": "/ercasale97/clicky-game/index.html"
+  }
+];
---
+++ build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/ercasale97/clicky-game/precache-manifest.de089df1a20aface5f1900d3a7b8f3d8.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/ercasale97/clicky-game/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
